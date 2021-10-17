04123dfb76f667dfefaa06a3af7ab53f14356a57
Wojtek <wojtek.koziol.pl@gmail.com>
2021-10-17 16:48:39 +0200

    Animate textbox

--- lib/animated_textbox.dart
+++ lib/animated_textbox.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AnimatedTextbox extends StatelessWidget {
+class AnimatedTextbox extends HookWidget {
   const AnimatedTextbox({Key? key}) : super(key: key);
 
   final _height = 70.0;
@@ -10,6 +11,23 @@ class AnimatedTextbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final animationController =
+        useAnimationController(duration: const Duration(milliseconds: 350));
+
+    final animation = Tween<double>(begin: 1, end: -1)
+        .chain(CurveTween(curve: Curves.fastOutSlowIn))
+        .animate(animationController);
+
+    final focusNode = useFocusNode();
+
+    useEffect(() {
+      focusNode.addListener(() {
+        focusNode.hasFocus
+            ? animationController.forward()
+            : animationController.reverse();
+      });
+    }, []);
+
     return SizedBox(
       height: _height * 2,
       width: _width,
@@ -26,8 +44,12 @@ class AnimatedTextbox extends StatelessWidget {
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomRight,
+          AnimatedBuilder(
+            animation: animation,
+            builder: (context, child) => Align(
+              alignment: Alignment(animation.value, 1),
+              child: child,
+            ),
             child: Padding(
               padding: EdgeInsets.only(left: _padding, right: _padding * 2),
               child: SizedBox(
@@ -35,22 +57,27 @@ class AnimatedTextbox extends StatelessWidget {
                 height: _height,
                 child: Center(
                   child: TextField(
+                    focusNode: focusNode,
                     decoration: InputDecoration(
+                      border: InputBorder.none,
                       hintText: 'Type a  name ...',
                       hintStyle: GoogleFonts.rubik(
                         color: Colors.black45,
                         fontSize: 18,
                         fontWeight: FontWeight.w500,
                       ),
-                      border: InputBorder.none,
                     ),
                   ),
                 ),
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
+          AnimatedBuilder(
+            animation: animation,
+            builder: (context, child) => Align(
+              alignment: Alignment(-1, animation.value),
+              child: child,
+            ),
             child: Padding(
               padding: const EdgeInsets.all(5),
               child: Container(
